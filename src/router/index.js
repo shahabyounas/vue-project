2927802277046a5f49b22b17e9ac5d9d3fbb0bc3
shahabyounas <younasshahab@gmail.com>
2024-06-25 15:48:24 +0100

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,10 @@ import RunwaysView from '../views/RunwaysView.vue'
 import ParkingView from '../views/ParkingView.vue'
 import MyDriveView from '../views/MyDriveView.vue'
 
+console.log('import.meta.env.BASE_URL', import.meta.env.BASE_URL)
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/vue-project'),
   routes: [
     {
       path: '/',
@@ -40,7 +42,8 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: HomeView }
   ]
 })
 
