045e45f5a47dea6c5f30e1fdc36b06136cfe4338
shahabyounas <younasshahab@gmail.com>
2024-06-25 15:36:12 +0100

    update config

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import RunwaysView from '../views/RunwaysView.vue'
 import ParkingView from '../views/ParkingView.vue'
 import MyDriveView from '../views/MyDriveView.vue'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -17,22 +16,22 @@ const router = createRouter({
     {
       path: '/airports',
       name: 'Airport',
-      component: AirportsView,
+      component: AirportsView
     },
     {
       path: '/runways',
       name: 'Runways',
-      component: RunwaysView,
+      component: RunwaysView
     },
     {
       path: '/mydrive',
       name: 'Mydrive',
-      component: MyDriveView,
+      component: MyDriveView
     },
     {
       path: '/parking',
       name: 'Parking',
-      component: ParkingView,
+      component: ParkingView
     },
     {
       path: '/about',
@@ -41,7 +40,7 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },
+    }
   ]
 })
 
